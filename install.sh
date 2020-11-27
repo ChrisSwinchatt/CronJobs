f50ba16c26aef16d2ebba847ad03dffbdfb6162a
Chris Swinchatt <chris@swinchatt.dev>
2020-11-27 15:21:03 +0000

    Don't let install.sh run as root

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -eq 0 ]]; then
+    echo "This script should not be run as root" >&2
+    exit 1
+fi
+
 PREFIX="${HOME}/.cronjobs"
 git clone -o "${PREFIX}" "https://github.com/ChrisSwinchatt/CronJobs"
 find "${PREFIX}" -name '*.sh' -exec chmod +x {} \;
